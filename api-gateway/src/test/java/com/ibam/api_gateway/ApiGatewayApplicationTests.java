ee9e929c65f62fb6c1f0c6aa5fbb3710863e0632
cooleliel <elielcoulibaly39@gmail.com>
2024-12-24 17:43:04 +0000

    initialisation des differents services

---
+++ api-gateway/src/test/java/com/ibam/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibam.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
