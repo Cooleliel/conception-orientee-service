fd932219afe73501dda527aded4ead1b16b245fd
cooleliel <elielcoulibaly39@gmail.com>
2025-01-07 17:39:55 +0000

    Implementation de la documentation des reponses rest avec swagger

---
+++ users-service/src/main/java/com/myportfolio/users_service/adapters/config/swagger/api_response/DefaultErrorApiResponse.java
@@ -0,0 +1,26 @@
+package com.myportfolio.users_service.adapters.config.swagger.api_response;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+    responseCode = "500",
+    description = "Erreur du serveur",
+    content = {
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = HttpErrorResponse.class)
+        )
+    }
+)
+public @interface DefaultErrorApiResponse {
+    
+}
---
+++ users-service/src/main/java/com/myportfolio/users_service/adapters/config/swagger/api_response/NotFoundApiResponse.java
@@ -0,0 +1,28 @@
+package com.myportfolio.users_service.adapters.config.swagger.api_response;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.myportfolio.users_service.adapters.config.exception.HttpErrorResponse;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+    responseCode = "404",
+    description = "Introuvable",
+    content = {
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = HttpErrorResponse.class)
+        )
+    }
+)
+public @interface NotFoundApiResponse {
+    
+}
