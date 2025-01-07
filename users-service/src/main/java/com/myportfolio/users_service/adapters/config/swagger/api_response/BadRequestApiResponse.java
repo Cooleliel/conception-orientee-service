c5e13169e43f3e3bf62fde60cd8838de9db45e94
cooleliel <elielcoulibaly39@gmail.com>
2025-01-07 17:56:51 +0000

    correction des imports

--- users-service/src/main/java/com/myportfolio/users_service/adapters/config/swagger/api_response/BadRequestApiResponse.java
+++ users-service/src/main/java/com/myportfolio/users_service/adapters/config/swagger/api_response/BadRequestApiResponse.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.myportfolio.users_service.adapters.config.exception.HttpErrorResponse;
+
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
