3afde9b5d82c94af8caee9f99ebfd2abe52ae56b
cooleliel <elielcoulibaly39@gmail.com>
2025-01-07 11:33:37 +0000

    refactorisation

--- products-service/src/main/java/com/ibam/products_service/usecases/product/command/BaseProductCommand.java
+++
@@ -1,8 +0,0 @@
-package com.ibam.products_service.usecases.product.command;
-
-public interface BaseProductCommand {
-    String name();
-    String description();
-    double price();
-    int stock();
-}
