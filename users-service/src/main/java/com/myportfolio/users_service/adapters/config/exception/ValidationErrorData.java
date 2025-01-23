61a9f5c7cd16d5ab8c24a34857739ecc3fc30786
cooleliel <elielcoulibaly39@gmail.com>
2025-01-23 23:23:42 +0000

    fix : amélioration de la gestion des erreurs de validation dans ValidationErrorData

--- users-service/src/main/java/com/myportfolio/users_service/adapters/config/exception/ValidationErrorData.java
+++ users-service/src/main/java/com/myportfolio/users_service/adapters/config/exception/ValidationErrorData.java
@@ -6,17 +6,16 @@ import java.util.List;
 
 import org.springframework.validation.FieldError;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 // Classe représentant les erreurs de validation
 @Builder
 public record ValidationErrorData(
-    @Schema(example = "users") String object,
-    @Schema(example = "email") String field,
-    @Schema(example = "{\\\"object\\\":\\\"users\\\",\\\"field\\\":\\\"username\\\",\\\"field\\\":\\\"email\\\",\\\"field\\\":\\\"password\\\"]}") 
+    /*@Schema(example = "users")*/ String object,
+    /*@Schema(example = "email")*/ String field,
+    /*@Schema(example = "{\\\"object\\\":\\\"users\\\",\\\"field\\\":\\\"username\\\",\\\"field\\\":\\\"email\\\",\\\"field\\\":\\\"password\\\"]}")*/ 
         Object rejectedValue,
-    @Schema(example = "[Format invalide]") List<String> messages
+    /*@Schema(example = "[Format invalide]")*/ List<String> messages
 ) {
     // Constructeur pour convertir une erreur de validation Spring en ValidationErrorData
     public ValidationErrorData(FieldError error) {
