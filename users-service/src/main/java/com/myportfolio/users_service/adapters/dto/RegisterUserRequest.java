2a758b88aa2720fe1de59221d6fd8b263e5b8afe
cooleliel <elielcoulibaly39@gmail.com>
2025-01-23 23:19:40 +0000

    refactor : mise à jour du DTO RegisterUserRequest avec de nouveaux champs

--- users-service/src/main/java/com/myportfolio/users_service/adapters/dto/RegisterUserRequest.java
+++ users-service/src/main/java/com/myportfolio/users_service/adapters/dto/RegisterUserRequest.java
@@ -1,14 +1,17 @@
 package com.myportfolio.users_service.adapters.dto;
 
 import com.myportfolio.users_service.usescases.user.command.BaseUserCommand;
+import com.myportfolio.users_service.utils.enums.RoleType;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Builder;
 
 // Requête d'enregistrement d'un utilisateur
 @Builder
 public record RegisterUserRequest(
-    @Schema(example = "johnman") String username,
-    @Schema(example = "john@example.com") String email,
-    @Schema(example = "motdepasse") String password
+    @Schema(example = "johnman", requiredMode = RequiredMode.REQUIRED) String username,
+    @Schema(example = "john@example.com", requiredMode = RequiredMode.NOT_REQUIRED) String email,
+    @Schema(example = "motdepasse", requiredMode = RequiredMode.REQUIRED) String password,
+    @Schema(example = "SELLER", requiredMode = RequiredMode.REQUIRED) RoleType role
 ) implements BaseUserCommand {} // Implémente le contrat pour les commandes utilisateur
