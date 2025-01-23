7ffed85f8da3c7c3cc02a37f9424a10b5075c9a9
cooleliel <elielcoulibaly39@gmail.com>
2025-01-23 23:06:14 +0000

    refactor : amélioration de la structure de la commande de base pour les opérations utilisateur

--- users-service/src/main/java/com/myportfolio/users_service/usescases/user/command/BaseUserCommand.java
+++ users-service/src/main/java/com/myportfolio/users_service/usescases/user/command/BaseUserCommand.java
@@ -1,8 +1,11 @@
 package com.myportfolio.users_service.usescases.user.command;
 
+import com.myportfolio.users_service.utils.enums.RoleType;
+
 // Interface définissant un contrat pour les commandes utilisateur
 public interface BaseUserCommand {
     String username(); // Nom d'utilisateur
     String email(); // Adresse e-mail
     String password(); // Mot de passe
+    RoleType role();
 }
